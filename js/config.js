8cea039cf6876bce05c6320e1eadd5208c97cb09
Michael Doe <ybmel001@gmail.com>
2025-11-18 21:58:36 -0500

    Update API URL

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   // API Base URL - automatically uses ngrok URL
   // js/config.js - line 3
-API_URL: 'https://seattle-car-chamber-specializing.trycloudflare.com/api',
+API_URL: 'https://podcasts-distinction-quoted-webmasters.trycloudflare.com/api',
   
   // Network Configuration
   NETWORK: 'bitcoin', // 'bitcoin' or 'testnet'
