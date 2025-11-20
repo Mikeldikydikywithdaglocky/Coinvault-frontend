3c4228386f60c7e54c526b79eef56ab4046e6cd7
Michael Doe <ybmel001@gmail.com>
2025-11-20 14:24:17 -0500

    Update API URL

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,7 @@
 
 
 // Dashboard.js - UPDATED with Dynamic Assets
-const API_URL = 'https://automobile-composition-struct-deviant.trycloudflare.com/api';
+const API_URL = 'https://runner-should-funeral-requests.trycloudflare.com/api';
 
 // Check authentication
 function checkDashboardAuth() {
