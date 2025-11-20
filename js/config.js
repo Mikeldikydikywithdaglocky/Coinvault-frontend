680fbc58f804de9a8c73f466d4a22d95a707eb54
Michael Doe <ybmel001@gmail.com>
2025-11-20 08:50:35 -0500

    Update API URL

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   // API Base URL - automatically uses ngrok URL
   // js/config.js - line 3
-API_URL: 'https://nightlife-onion-transmitted-transform.trycloudflare.com/api',
+API_URL: 'https://driven-protected-encourage-clothing.trycloudflare.com/api',
   
   // Network Configuration
   NETWORK: 'bitcoin', // 'bitcoin' or 'testnet'
