1a5141ccff6c978d61661c71cd61e82504fba18c
Michael Doe <ybmel001@gmail.com>
2026-02-17 08:34:09 -0500

    api added to link

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
   // API Base URL - automatically uses ngrok URL
   // js/config.js - line 3
-API_URL: 'https://lauren-grade-limiting-cursor.trycloudflare.com',
+API_URL: 'https://lauren-grade-limiting-cursor.trycloudflare.com/api',
   
   // Network Configuration
   NETWORK: 'bitcoin', // 'bitcoin' or 'testnet'
